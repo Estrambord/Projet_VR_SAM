5ea0bbe70866fd83a1d47d225b90917856ae879c
Hugo RICHIR <hugo.richir@hotmail.fr>
2021-01-05 13:03:43 +0100

    Hugo Start
    Test Debut

---
+++ Assets/Script/PartyManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PartyManager : MonoBehaviour
+{
+    private int roundNb = 0;
+
+    public TMP_Text instructionText;
+    public TMP_Text roundText;
+    public TMP_Text timerText;
+    public Round introduction;
+
+    public GameObject panel1;
+    public GameObject panel2;
+    public GameObject panel3;
+
+    public bool SlotPlayer1 = false;
+    public bool SlotPlayer2 = false;
+    public bool SlotPlayer3 = false;
+
+    void Start()
+    {
+        ChooseRole();
+    }
+
+    /*
+    void Update()
+    {
+        
+    }
+    */
+
+    void ChooseRole()
+    {
+        PlayRound(introduction);
+        panel1.SetActive(true); panel2.SetActive(true); panel3.SetActive(true);
+        //if skeleton enter zone --> slotRole = ok
+        //if slot1 && slot2 && slot3 == ok --> Press DownArrowToContinue
+        //Valid Slots & Pass to next Step
+    }
+
+    void PlayRound(Round myRound)
+    {
+        instructionText.text = myRound.instructions;
+        if (myRound.isGame) { roundNb++; roundText.text = roundNb.ToString(); }
+        else roundText.text = "";
+        if (myRound.timer > 0f) timerText.text = "";
+        else timerText.text = myRound.timer.ToString();
+    }
+}
---
+++ Assets/Script/Round.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Round", menuName = "Round")]
+public class Round : ScriptableObject
+{
+    public string instructions;
+    public bool isGame;
+    public float timer;
+    public GameObject firstMur;
+    public GameObject secondMur;
+    public GameObject thirdMur;
+}
